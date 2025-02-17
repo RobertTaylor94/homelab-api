2f3ac13904609ea8fa22fe3762704f56a02f19fc
Robert Taylor <rjt903@gmail.com>
2025-02-17 10:29:08 +0000

    updated step db query to update with new data

--- handlers/healthkit_util.go
+++ handlers/healthkit_util.go
@@ -29,7 +29,7 @@ func saveStepData(db *sql.DB, d *models.StepCountDaily) error {
 		return fmt.Errorf("failed to parse timestamp: %v", err)
 	}
 
-	query := `INSERT INTO healthkit.step_data (recorded_at, quantity) VALUES ($1, $2) ON CONFLICT (recorded_at) DO NOTHING`
+	query := `INSERT INTO healthkit.step_data (recorded_at, quantity) VALUES ($1, $2) ON CONFLICT (recorded_at) DO UPDATE SET quantity = EXCLUDED.quantity`
 	_, err = db.Exec(query, parsedTime, math.Round(float64(d.Quantity)))
 	if err != nil {
 		return fmt.Errorf("failed to save step data: %v", err)
